9c52d296f5d84c80d73854bb94ee94d68b1d2b7f
Sergey <levashov04sergey@gmail.com>
2021-11-03 22:14:53 +0200

    create  interface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import styles from './App.module.css';
 
 const App = () => {
-  return (
-      <div>
-        
-      </div>
-  );
+    const [time, setTime] = useState(0);
+    const [timerOn, setTimerOn] = useState(false);
+    const [wait, setWait] = useState(false);
+
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <span>{("0" + Math.floor(time / 60000) % 60).slice(-2)} : </span>
+                <span>{("0" + Math.floor(time / 1000) % 60).slice(-2)} : </span>
+                <span>{("0" + (time / 10) % 100).slice(-2)}</span>
+            </div>
+            <div>
+                {!timerOn && (
+                    <button onClick={() => setTimerOn(true)}>Start</button>
+                )}
+                {timerOn && (
+                    <button onClick={() => setTimerOn(false)}>Stop</button>
+                )}
+                <button  onClick={()=> setWait(true)}>Wait</button>
+                <button onClick={() => setTime(0)}>Reset</button>
+            </div>
+
+        </div>
+    );
 };
 
 export default App;
